f2a6dadbc2a3c23145b9dfad5cab4225fa1e5569
gscarborough <gigimscarborough@gmail.com>
2022-02-15 14:17:38 -0500

    hotfix documentation language

--- __tests__/03_surrender_now.test.js
+++ __tests__/03_surrender_now.test.js
@@ -6,3 +6,62 @@ const {
 const pokemon = require("../data/poke");
 const shuffledPokemon = require("../data/poke_remix");
 const weaknesses = require("../data/weaknesses");
+
+describe("filterByGenre()", () => {
+    test("should return all pokemon that include the specified type", () => {
+        const type = "Mystery";
+        const actual = filterByType(pokemon, type);
+        const expected = [
+            pokemon[2], // Coco
+        ];
+        expect(actual).toEqual(expected);
+    });
+    test("should dynamically change depending on the type inputted", () => {
+        const type = "Fantasy";
+        const actual = filterByType(pokemon, type);
+        const expected = [
+            pokemon[0], // Toy Story 4
+            pokemon[1], // Inside Out
+            pokemon[2], // Coco
+            pokemon[5], // How to Train Your Dragon
+            pokemon[8], // Fantasia
+            pokemon[9], // James and the Giant Peach
+        ];
+        expect(actual).toEqual(expected);
+    });
+    test("should dynamically change depending on the pokemon inputted", () => {
+        const type = "Action";
+        const actual = filterByType(alternative, type);
+        const expected = [
+            alternative[0], // Black Panther
+            alternative[1], // Wonder Woman
+            alternative[3], // Skyfall
+        ];
+        expect(actual).toEqual(expected);
+    });
+    test("should be case-insensitive", () => {
+        const type = "FANTASY";
+        const actual = filterByType(pokemon, type);
+        const expected = [
+            pokemon[0], // Toy Story 4
+            pokemon[1], // Inside Out
+            pokemon[2], // Coco
+            pokemon[5], // How to Train Your Dragon
+            pokemon[8], // Fantasia
+            pokemon[9], // James and the Giant Peach
+        ];
+        expect(actual).toEqual(expected);
+    });
+    test("should return an empty array if no pokemon match the type", () => {
+        const type = "Horror";
+        const actual = filterByType(pokemon, type);
+        const expected = [];
+        expect(actual).toEqual(expected);
+    });
+    test("should return an empty array if there are no pokemon", () => {
+        const type = "Action";
+        const actual = filterByType([], type);
+        const expected = [];
+        expect(actual).toEqual(expected);
+    });
+});
--- src/03_surrender_now.js
+++ src/03_surrender_now.js
@@ -10,13 +10,13 @@ const exampleWeaknesses = require("../data/weaknesses");
 /**
  * filterByType()
  * -----------------------------
- * Returns all Pokemon objects with a matching genre. Case-insensitive. If the inputted `movies` array is empty or no movies match the inputted `type`, return `[]`.
+ * Returns all Pokemon objects with a matching `type`. Case-insensitive. If the inputted `pokemon` array is empty or no Pokemon match the inputted `type`, return `[]`.
  * @param {Object[]} pokemon - An array of Pokemon. See the `poke.js` file for an example of this array.
  * @param {string} type - The type of a Pokemon. (e.g. "Fire")
  * @returns {Object[]} An array of Pokemon where at least one of the types matches the `type` inputted.
  *
  * EXAMPLE:
- *  filterByType(pokemon, "Steel");
+ *  filterByType(pokemon, "steel");
  *  //> [
       {
         // Coco
